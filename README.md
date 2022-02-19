a689fbaac2b50b0a10c390d014a896c6162ab570
E4tHam <ethan@sifferman.dev>
2022-02-19 11:13:24 -0800

    added light documentation

---
+++ README.md
@@ -0,0 +1,20 @@
+# Data Fusion
+
+<https://github.com/distributed-radar/data-fusion>
+
+## Class Structure
+
+* RadarNode
+  * Observation List
+  * Neighbor List
+* Observation
+  * Position and doppler data
+* Neighbor
+  * RadarNode
+  * Position and view angle
+
+## Example
+
+This is the output of `TestOnePoint.m`.
+
+![TestOnePoint](TestOnePoint.png)
--- TestOnePoint.m
+++ TestOnePoint.m
@@ -2,14 +2,16 @@
 r1 = RadarNode([], []);
 r2 = RadarNode([], []);
 
+% Change parameters as desired
 r2.observations = [Observation((pi/2),1,0,0)];
-
 r1.neighbors = [Neighbor(0,2,pi,r2)];
+%
 
+% Generate figures
 figure(2);
+hold on;
 title('Radar 2 POV');
 scatter(0,0,'DisplayName','R2 Location');
-hold on;
 [obs_x,obs_y] = pol2cart(r2.observations(1).position_angle,r2.observations(1).position_distance);
 scatter(obs_x,obs_y,'DisplayName','R2 Observation');
 quiver(0,0,1,0,'DisplayName','View Angle');
@@ -21,10 +23,10 @@ ylim([-5 5]);
 hold off;
 
 figure(1);
+hold on;
 title('Radar 1 POV');
 [neighb_x,neighb_y] = pol2cart(r1.neighbors(1).position_angle,r1.neighbors(1).position_distance);
 scatter(neighb_x,neighb_y,'DisplayName','R2 Location');
-hold on;
 neighbor_observations = r1.getNeighborObservations();
 [obs_x,obs_y] = pol2cart(neighbor_observations(1).position_angle,neighbor_observations(1).position_distance);
 scatter(obs_x,obs_y,'DisplayName','R2 Observation');
